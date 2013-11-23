4e7adf1ce1a55f4c599cade52d3828c774012d64
Adrian <adrian.delarosab@gmail.com>
2013-11-23 17:23:26 +0100

    .gitignore updated

--- application/config/constant.php
+++ application/config/constant.php
@@ -2,10 +2,10 @@
 
 /**
  *
- * @author       Adrian de la Rosa Bretin
- * @version      1.0 (10/10/13)
+ * @author Adrian de la Rosa Bretin
+ * @version 1.0 (10/10/13)
  *
- * @copyright    La Cuarta Edad
+ * @copyright 2013 La Cuarta Edad
  *
  */
 
